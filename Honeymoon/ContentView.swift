a63bed2c665e1fae5be823bcf26a1147e8d46259
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-01-24 23:23:03 -0500

    Initial Commit

---
+++ Honeymoon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
